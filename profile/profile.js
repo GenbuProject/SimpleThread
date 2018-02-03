ad837d178380998a25f6c4308ea02d1ef2190ee4
Genbu Hase <hasegenbu@gmail.com>
2018-02-03 14:13:11 +0900

    未ログイン時にプロフィール画面を封印するように。

--- profile/profile.js
+++ profile/profile.js
@@ -1,17 +1,34 @@
 terminal.addEventListener("message", event => {
 	if (event.data.code == "Loaded") {
-		base.Database.get(base.Database.INTERVAL, `users/${base.user.uid}`, res => {
-			new DOM("#Profile-Info-Name").value = res.userName,
-			new DOM("#Profile-Info-Name").parentNode.querySelector("Label").classList.add("mdc-text-field__label--float-above");
+		if (base.user) {
+			base.Database.get(base.Database.INTERVAL, `users/${base.user.uid}`, res => {
+				new DOM("#Profile-Info-Name").value = res.userName,
+				new DOM("#Profile-Info-Name").parentNode.querySelector("Label").classList.add("mdc-text-field__label--float-above");
 
-			new DOM("#Profile-Info-Detail").value = res.detail,
-			new DOM("#Profile-Info-Detail").parentNode.querySelector("Label").classList.add("mdc-text-field__label--float-above");
-		});
+				new DOM("#Profile-Info-Detail").value = res.detail,
+				new DOM("#Profile-Info-Detail").parentNode.querySelector("Label").classList.add("mdc-text-field__label--float-above");
+			});
+		} else {
+			location.href = "/SimpleThread/";
+		}
 	}
 });
 
 window.addEventListener("DOMContentLoaded", () => {
 	new DOM("@.mdc-text-field").forEach(textField => new mdc.textField.MDCTextField(textField));
+
+	let deleteConfirmer = new mdc.dialog.MDCDialog(new DOM("#DeleteConfirmer"));
+		deleteConfirmer.listen("MDCDialog:accept", () => {
+			base.delete();
+		});
+
+	new DOM("#DeleteConfirmer-Email").addEventListener("input", event => {
+		if (event.target.value == base.user.email) {
+			deleteConfirmer.acceptButton_.removeAttribute("disabled");
+		} else {
+			deleteConfirmer.acceptButton_.setAttribute("disabled", "");
+		}
+	});
 
 	new DOM("#Profile-Save").addEventListener("click", () => {
 		base.Database.update("users/" + base.user.uid, {
@@ -23,6 +40,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	});
 
 	new DOM("#Profile-Delete").addEventListener("click", () => {
-		new mdc.dialog.MDCDialog(new DOM("#DeleteConfirmer")).show();
+		deleteConfirmer.show();
 	})
 });
